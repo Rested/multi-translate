ea8de1204d5a8504a73dc0f7972c26b2b8a102d8
Reuben Thomas-Davis <reubenestd@gmail.com>
2020-06-21 14:38:40 +0100

    :sparkles: add gql functional test

---
+++ functional_tests/translate/test_gql_endpoint.py
@@ -0,0 +1,64 @@
+from urllib.parse import urljoin
+
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+
+from functional_tests.ft_settings import FTSettings
+
+base = FTSettings().rest_url
+
+
+def test_graphql_works():
+    transport = RequestsHTTPTransport(
+        url=urljoin(base, "gql"),
+        verify=False,
+        retries=3,
+    )
+
+    client = Client(
+        transport=transport,
+        fetch_schema_from_transport=True,
+    )
+    query = gql('''
+    query GetTranslation {
+        translation(sourceText: "How do you do?", toLanguage: "es", withAlignment: true) {
+            translatedText
+            fromLanguage
+            alignment{
+              src{
+                start,
+                end,
+                text
+              }
+              dest{
+                start,
+                end,
+                text
+              }
+            }
+      }
+    }
+    ''')
+
+    result = client.execute(query)
+
+    assert result == {
+        "translation": {
+            "translatedText": "¿Cómo estás?",
+            "fromLanguage": "en",
+            "alignment": [
+                {
+                    "src": {
+                        "start": 0,
+                        "end": 13,
+                        "text": "How do you do?"
+                    },
+                    "dest": {
+                        "start": 0,
+                        "end": 11,
+                        "text": "¿Cómo estás?"
+                    }
+                }
+            ]
+        }
+    }
